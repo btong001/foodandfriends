98a97b49d5afe53d9d5a97c60e758c8db5d256bb
btong001 <brandontong436@gmail.com>
2025-12-17 17:59:22 -0500

    Intiialize spring

---
+++ backend/src/main/java/com/brandon/foodfriends/FoodfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.foodfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodfriendsApplication.class, args);
+	}
+
+}
